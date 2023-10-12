ad1dca98e52c1d6ecd804b95a48b2c0943b99c46
sim3.14 <iurie.cius.personal@gmail.com>
2023-10-12 11:18:29 +0300

    feat(auth-service): implement jwt strategy

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -12,6 +12,5 @@ async function bootstrap() {
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
-  await app.listen(3000);
 }
 bootstrap();
