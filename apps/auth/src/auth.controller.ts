9ffb5294741879ec9a14eefb70be52f966d531e9
sim3.14 <iurie.cius.personal@gmail.com>
2023-10-12 13:01:05 +0300

    feat(auth-service): add common auth guard + TCP connection

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Res, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { CurrentUser } from './current-user.decorator';
+import { CurrentUser } from '@app/common';
 import { UserDocument } from './users/models/user.schema';
 import { Response } from 'express';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,12 @@ export class AuthController {
     await this.authService.login(user, response);
 
     response.send(user);
+  }
+
+  // Accept Incoming RPC calls
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('authenticate')
+  async authenticate(@Payload() data: any) {
+    return data.user;
   }
 }
