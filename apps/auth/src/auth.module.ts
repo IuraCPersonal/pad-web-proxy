0277ec73d8e1362ba5a26db40eaa081a22b73989
sim3.14 <iurie.cius.personal@gmail.com>
2023-10-12 10:15:35 +0300

    feat(auth-service): create local strategy

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -7,6 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -32,6 +33,6 @@ import { ConfigModule } from '@nestjs/config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
