d93a2e7e91e7ff7858a113dab3d8a36403e55253
sim3.14 <iurie.cius.personal@gmail.com>
2023-10-17 15:18:49 +0300

    feat(payments-service): setup stripe

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreatePaymentDto } from '../dto/create-payment.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2023-10-16',
+    },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async createPayment({ card, amount }: CreatePaymentDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
+  }
+}
