305c9ea169cc08a771c79e2a9f2be99e67c901cf
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-10-26 09:19:01 +0300

    chore(gateway): setup endpoints

--- gateway/main.py
+++ gateway/main.py
@@ -1,29 +1,35 @@
+import os
+
 import httpx
+import redis
+
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from pybreaker import CircuitBreaker, CircuitBreakerError
+
+from pybreaker import CircuitBreaker, CircuitRedisStorage, STATE_CLOSED
 
 from models.user import User
 from models.health import HealthCheck
 
 app = FastAPI()
 
-# Create a Circuit Breaker
-circuit_breaker = CircuitBreaker(
-    fail_max=3,   # Maximum number of consecutive failures before opening the circuit
-    reset_timeout=30,  # Time in seconds to wait before attempting to close the circuit again
+redis = redis.StrictRedis()
+db_breaker = CircuitBreaker(
+    fail_max=5,
+    reset_timeout=60,
+    state_storage=CircuitRedisStorage(STATE_CLOSED, redis)
 )
 
-
-@app.exception_handler(CircuitBreakerError)
-async def handle_circuit_breaker_error(request, exc):
-    return JSONResponse(content={"error": "Circuit is open"}, status_code=503)
+REDIS_HOST = os.getenv('REDIS_HOST') or "redis-cache"
+REDIS_PORT = os.getenv('REDIS_PORT') or 6379
 
 
-@app.get("/simulate-failure")
-def simulate_failure(status_code=status.HTTP_503_SERVICE_UNAVAILABLE):
-    return status.HTTP_503_SERVICE_UNAVAILABLE
+def cache():
+    return redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT
+    )
 
 
 @app.get(
@@ -34,7 +40,6 @@ def simulate_failure(status_code=status.HTTP_503_SERVICE_UNAVAILABLE):
     status_code=status.HTTP_200_OK,
     response_model=HealthCheck,
 )
-@circuit_breaker
 async def get_health() -> HealthCheck:
     """
     ## Perform a Health Check
@@ -46,3 +51,38 @@ async def get_health() -> HealthCheck:
         HealthCheck: Returns a JSON response with the health status
     """
     return HealthCheck(status="OK")
+
+
+@app.post(
+    "/users"
+)
+async def create_user(new_user):
+    return 201
+
+
+@app.post(
+    "/auth/login"
+)
+async def login(credentials):
+    return 200
+
+
+@app.get(
+    "/reservations"
+)
+async def get_reservations():
+    return 200
+
+
+@app.post(
+    "/reservations"
+)
+async def create_reservation(new_reservation):
+    return 201
+
+
+@app.get(
+    "/reservations/{reservation_id}"
+)
+async def get_reservation_details():
+    return 200
